a5e2f56b0b3fa5e4490fbcc9506607db008bbf6a
Samtlokomemo <samuel.maranhao9@gmail.com>
2026-02-06 17:13:40 -0300

    DOAKES

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 const meuBotao = document.querySelector("#BOTAODIVERTIDO");
-const divImg = document.querySelector("#cont-crias")
+const login = document.querySelector("#INPUTLOGIN");
+const senha = document.querySelector("#INPUTSENHA");
+const divImg = document.querySelector("#cont-crias");
+const divImg2 = document.querySelector("#cont-crias2");
+const video = document.querySelector("#doakes")
 const yay = new Audio("yay.mp3");
+
+function tocarVideo(){
+    video.style.display = "block";
+    video.play();
+}
 
 let tocando = false;
 meuBotao.addEventListener("click", function(){
+    if(login.value != "marcos" && senha.value != "1234"){
+        alert("BURRO")
+        return
+    }
     if(tocando == true){
         return;
     }
     tocando = true;
+    setTimeout(tocarVideo, 3000);
     yay.currentTime = 0;
     yay.play();
     divImg.classList.add("visivel");
-});
+    divImg2.classList.add("visivel2");
+});  
 
 yay.addEventListener("ended", (event) => {
     divImg.classList.remove("visivel");
+    divImg2.classList.remove("visivel2");
     tocando = false;
 });
+
