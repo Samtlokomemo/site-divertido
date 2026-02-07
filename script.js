16a68c9a5c791a29e651350af15655964b76c4ec
Samuel Rocha <samuel.maranhao9@gmail.com>
2026-02-07 14:30:35 -0300

    Site final

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
-const meuBotao = document.querySelector("#BOTAODIVERTIDO");
+const botao = document.querySelector("#BOTAODIVERTIDO");
 const login = document.querySelector("#INPUTLOGIN");
+const loginDiv = document.querySelector(".LOGIN");
+const loginPDiv = document.querySelector(".LOGIN_PEQUENO");
 const senha = document.querySelector("#INPUTSENHA");
-const divImg = document.querySelector("#cont-crias");
-const divImg2 = document.querySelector("#cont-crias2");
 const video = document.querySelector("#doakes")
-const yay = new Audio("yay.mp3");
+const msgErro = document.querySelector("#msg-erro");
+const modal = document.querySelector("#modal-ajuda");
+const btnAjuda = document.querySelector("#btn-ajuda");
+const spanClose = document.querySelector(".close");
 
-function tocarVideo(){
-    video.style.display = "block";
-    video.play();
-}
+// Controle do Modal
+btnAjuda.onclick = () => modal.style.display = "block";
+spanClose.onclick = () => modal.style.display = "none";
+window.onclick = (event) => { if (event.target == modal) modal.style.display = "none"; }
 
-let tocando = false;
-meuBotao.addEventListener("click", function(){
-    if(login.value != "marcos" && senha.value != "1234"){
-        alert("BURRO")
-        return
-    }
-    if(tocando == true){
+botao.addEventListener("click", function () {
+    const userVal = login.value.trim();
+    const senhaVal = senha.value.trim();
+
+    if (userVal !== "marcos" || senhaVal !== "1234") {
+        msgErro.innerText = "Dados inválidos!";
+        msgErro.classList.add("erro-animacao");
+
+        setTimeout(() => msgErro.classList.remove("erro-animacao"), 400);
         return;
     }
-    tocando = true;
-    setTimeout(tocarVideo, 3000);
-    yay.currentTime = 0;
-    yay.play();
-    divImg.classList.add("visivel");
-    divImg2.classList.add("visivel2");
-});  
 
-yay.addEventListener("ended", (event) => {
-    divImg.classList.remove("visivel");
-    divImg2.classList.remove("visivel2");
-    tocando = false;
+    setTimeout(() => {
+        video.style.display = "block";
+        video.play();
+    }, 1000);
+
+    loginDiv.innerHTML = `
+        <div class="banner-meio-container">
+            <img src="res/banner1.gif" alt="banner">
+            <img src="res/banner2.gif" alt="banner">
+            <img src="res/banner7.gif" alt="banner">
+            <img src="res/banner4.gif" alt="banner">
+            <img src="res/banner5.gif" alt="banner">
+            <img src="res/banner6.gif" alt="banner">
+        </div>
+    `;
 });
 
+video.addEventListener("ended", () => {
+    video.remove();
+});
